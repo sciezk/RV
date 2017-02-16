6f6e0db63a498f54762c9ae96d5e94968e2e6344
saul <sciezk@gmail.com>
2017-02-16 14:23:53 -0600

    Update rtcf.js

--- rtcf.js
+++ rtcf.js
@@ -4,4 +4,4 @@
   contexto.fillRect(10,10,60,60);
     var contexto2 = mi2Canvas.getContext("2d");
   contexto2.fillStyle="yellow";
-  contexto2.fillRect(1,1,25,25);
+  contexto2.fillRect(12,12,25,25);
