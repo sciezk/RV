0a79d874003fa861532cea79c038b56de6abc7d8
saul <sciezk@gmail.com>
2017-02-21 14:14:07 -0600

    Update GM2017221,1.js

--- Mapo/GM2017221,1.js
+++ Mapo/GM2017221,1.js
@@ -1,11 +1,4 @@
-var escena = new THREE.Scene();
-escena.add( malla );
+var material = new THREE.MeshNormalMaterial();
 
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
-
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, 
-                      window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
