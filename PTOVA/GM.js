b5e6febbf19057fbaa236143ef439fb48a136998
saul <sciezk@gmail.com>
2017-02-23 14:01:45 -0600

    Update GM.js

--- PTOVA/GM.js
+++ PTOVA/GM.js
@@ -1,19 +1,25 @@
-var figura = new THREE.Shape();
-figura.moveTo(10, 10);
+//revolucion extrsion vertices composicion
+var puntos = [];
+for ( var i = 0; i < 50; i ++ ) {
+    puntos.push( new THREE.Vector2(
+                     Math.sin( i * 0.2 ) * 15 + 50,
+                     ( i - 5 ) * 2 ) );
+}
+var forma = new THREE.LatheGeometry(puntos);
 
-figura.RoundedRect().setSize(100,40).
-    setRadius(10);
+var material = new THREE.MeshNormalMaterial();
 
-var forma = new THREE.ShapeGeometry(figura);
-var malla = new THREE.Mesh(forma);
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX( Math.PI/6 );
 var escena = new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 100;
+camara.position.z = 500;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95,
-                      window.innerHeight*.95 );
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
+
+
