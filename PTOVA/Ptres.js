ea3818fde38b72b851a24750f5074abbba9ab285
saul <sciezk@gmail.com>
2017-03-05 22:14:17 -0600

    Update Ptres.js

--- PTOVA/Ptres.js
+++ PTOVA/Ptres.js
@@ -1,15 +1,12 @@
 var puntos = [];
 for ( var i=0; i<50; i++){
-  puntos.push(new THREE.Vector2(Math.sin(i*0.2)*15+50, (i-5)*2));
+  puntos.push(new THREE.Vector2(Math.cos(i*0.2)*15+50, (i-5)*2));
 }
 
 var forma = new THREE.LatheGeometry(puntos);
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh(forma, material);
-//malla.rotateX(Math.PI/6);
-//malla.position.x = 400;
-//malla.position.y = - 250;
 malla.rotateX(Math.PI / 2);
 
 var escena=new THREE.Scene();
