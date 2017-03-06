f7bdceab3186d9bc7375ef0c2f50915e5503a467
saul <sciezk@gmail.com>
2017-03-05 23:33:14 -0600

    Update Psi.js

--- PTOVA/Psi.js
+++ PTOVA/Psi.js
@@ -12,7 +12,7 @@ malla.rotateX(Math.PI / 2);
 var escena=new THREE.Scene();
 escena.add(malla);
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=500;
+camara.position.z=100;
 renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
