2bad6f21de8346e44647569d492e7f3bdbc0f421
saul <sciezk@gmail.com>
2017-03-05 22:26:10 -0600

    Update Pdos.js

--- PTOVA/Pdos.js
+++ PTOVA/Pdos.js
@@ -1,16 +1,20 @@
 var forma = new THREE.Geometry();
 
-forma.vertices.push( new THREE.Vector3( 1, 0, 1) );
-forma.vertices.push( new THREE.Vector3( 1, 0, -1) );
-forma.vertices.push( new THREE.Vector3( -1, 0, -1) );
-forma.vertices.push( new THREE.Vector3( -1, 0, 1) );
-forma.vertices.push( new THREE.Vector3( 0, 1, 0) );
+forma.vertices.push( new THREE.Vector3( 1, 0, 1,1) );
+forma.vertices.push( new THREE.Vector3( 1, 0, -1,1) );
+forma.vertices.push( new THREE.Vector3( -1, 0, -1,1) );
+forma.vertices.push( new THREE.Vector3( -1, 0, 1,1) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 0,1) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 0,1) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 0,1) );
 
 forma.faces.push( new THREE.Face3( 3, 2, 1 ) );
 forma.faces.push( new THREE.Face3( 3, 1, 0 ) );
 forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
 forma.faces.push( new THREE.Face3( 0, 1, 4 ) );
 forma.faces.push( new THREE.Face3( 1, 2, 4 ) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
 forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
 
 forma.computeBoundingSphere();
