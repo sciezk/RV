33ea1093a002e65b8a9f4948d6a3268f61df1691
saul <sciezk@gmail.com>
2017-03-05 23:20:39 -0600

    Update Pdos.js

--- PTOVA/Pdos.js
+++ PTOVA/Pdos.js
@@ -1,9 +1,12 @@
 var forma = new THREE.Geometry();
 
-forma.vertices.push( new THREE.Vector3( -1.0,  1.0, 0.0) );
-forma.vertices.push( new THREE.Vector3( 1.0,  1.0, 0.0) );
-forma.vertices.push( new THREE.Vector3( 1.0, -1.0, 0.0) );
-forma.vertices.push( new THREE.Vector3(-1.0, -1.0, 0.0) );
+forma.vertices.push( new THREE.Vector3( 2,7,0) );
+forma.vertices.push( new THREE.Vector3(7,2,0) );
+forma.vertices.push( new THREE.Vector3( 12,7,0]) );
+forma.vertices.push( new THREE.Vector3(12,17,0) );
+forma.vertices.push( new THREE.Vector3(7,12,0) );
+forma.vertices.push( new THREE.Vector3(2,17,0) );
+forma.vertices.push( new THREE.Vector3(2,7,0) );
 
 
 forma.faces.push(new THREE.Face4(0, 1, 2, 3));
