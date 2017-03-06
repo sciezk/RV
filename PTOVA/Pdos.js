fbdf79c52f7ef46eaff64ac5a94191dda28ba4d2
saul <sciezk@gmail.com>
2017-03-05 22:28:20 -0600

    Update Pdos.js

--- PTOVA/Pdos.js
+++ PTOVA/Pdos.js
@@ -5,17 +5,17 @@ forma.vertices.push( new THREE.Vector3( 1, 0, -1,1) );
 forma.vertices.push( new THREE.Vector3( -1, 0, -1,1) );
 forma.vertices.push( new THREE.Vector3( -1, 0, 1,1) );
 forma.vertices.push( new THREE.Vector3( 0, 1, 0,1) );
-forma.vertices.push( new THREE.Vector3( 0, 1, 0,1) );
-forma.vertices.push( new THREE.Vector3( 0, 1, 0,1) );
+forma.vertices.push( new THREE.Vector3( 0, 2, 0,1) );
+forma.vertices.push( new THREE.Vector3( 0, 1, 0,2) );
 
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) );
-forma.faces.push( new THREE.Face3( 3, 1, 0 ) );
-forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
-forma.faces.push( new THREE.Face3( 0, 1, 4 ) );
-forma.faces.push( new THREE.Face3( 1, 2, 4 ) );
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
-forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
+forma.faces.push( new THREE.Face3( 3, 2, 1 ,4) );
+forma.faces.push( new THREE.Face3( 3, 1, 0,4 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 4 ,5) );
+forma.faces.push( new THREE.Face3( 0, 1, 4,5 ) );
+forma.faces.push( new THREE.Face3( 1, 2, 4,5 ) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ,5) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ,5) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ,5) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
