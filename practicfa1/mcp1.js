a0b6409f03e0b30299c067f6021359a2c2017df4
saul <sciezk@gmail.com>
2017-03-16 13:32:03 -0600

    Create mcp1.js

---
+++ practicfa1/mcp1.js
@@ -0,0 +1,26 @@
+var torreForma = new THREE.CylinderBufferGeometry(5, 5, 20, 32);
+var esferaForma = new THREE.SphereGeometry(.65);
+var raizforma =  new THREE.BoxGeometry( 1, 1, 1 );
+esferaForma.translate(0,1,0);
+
+var torreMalla = new THREE.Mesh(torreForma);
+var esferaMalla = new THREE.Mesh(esferaForma);
+var raizMalla = new THREE.Mesh(raizForma);
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+
+var hongo = new THREE.Geometry();
+hongoForma.merge(torreMalla.geometry, torreMalla.matrix);
+hongoForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+hongoForma.merge(raizMalla.geometry, raizMalla.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var hongoMalla = new THREE.Mesh(hongoForma, material);
+var escena = new THREE.Scene();
+escena.add(hongoMalla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
