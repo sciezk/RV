c46a27d4aa9a51b72e5a7c36c26a39f56833aa5b
saul <sciezk@gmail.com>
2017-03-17 20:48:38 -0600

    Create mcp2.js

---
+++ practicfa1/mcp2.js
@@ -0,0 +1,31 @@
+var troncoForma = new THREE.CylinderGeometry(3,3,6,20,6,false);
+var cubierta1Forma = new THREE. BoxGeometry(4,3,2);
+var cubierta2Forma = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+
+troncoForma.translate(0,4,0);
+cubierta1Forma.translate(0,8,0);
+cubierta2Forma.translate(0,10,0);
+
+var toncoMalla= new THREE.Mesh(troncoForma);
+var cubierta1Malla= new THREE.Mesh(cubierta1Forma);
+var cubierta2Malla= new THREE.Mesh(cubierta2Forma);
+
+var torreForma = new THREE.Geometry();
+
+torreForma.merge(toncoMalla.geometry, toncoMalla.matrix);
+torreForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
+torreForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
+
+var material= new THREE.MeshNormalMaterial();
+var torreMalla = new THREE.Mesh(torreForma, material);
+
+torreMalla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add(torreMalla);
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=50;
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
