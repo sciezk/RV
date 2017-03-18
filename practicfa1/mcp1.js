f1168ee717e90cb811cf23d615b7064f438eb260
saul <sciezk@gmail.com>
2017-03-18 00:18:59 -0600

    Update mcp1.js

--- practicfa1/mcp1.js
+++ practicfa1/mcp1.js
@@ -1,18 +1,22 @@
-var troncoForma = new THREE.CylinderGeometry(1,2,6,10,6,false);
-var cubierta1Forma = new THREE. BoxGeometry(4,3,2);
+var troncoForma = new THREE.CylinderGeometry(1,2,10,10,6,false);
+var cubierta1Forma = new THREE. BoxGeometry(4,3,0.7);
 var cubierta2Forma = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+var conForma = new THREE.CylinderGeometry(1,2,4,10,6,false);
 
 troncoForma.translate(0,6,0);
+conForma.translate(0,8,0);
 cubierta1Forma.translate(0,1,0);
 cubierta2Forma.translate(0,10,0);
 
 var toncoMalla= new THREE.Mesh(troncoForma);
+var conMalla= new THREE.Mesh(conForma);
 var cubierta1Malla= new THREE.Mesh(cubierta1Forma);
 var cubierta2Malla= new THREE.Mesh(cubierta2Forma);
 
 var torreForma = new THREE.Geometry();
 
 torreForma.merge(toncoMalla.geometry, toncoMalla.matrix);
+torreForma.merge(conMalla.geometry, conMalla.matrix);
 torreForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
 torreForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
 
