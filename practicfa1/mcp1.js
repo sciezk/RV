6079dec0e8988f862360c59905a33afa1da1f890
saul <sciezk@gmail.com>
2017-03-18 14:21:32 -0600

    Update mcp1.js

--- practicfa1/mcp1.js
+++ practicfa1/mcp1.js
@@ -1,9 +1,10 @@
 var troncoForma = new THREE.CylinderGeometry(1.5,2,13,10,6,false);
 var cubierta1Forma = new THREE. BoxGeometry(7,.2,7);
-var cubierta2Forma = new THREE.SphereGeometry(3, 21, 21);
+var cubierta2Forma = new THREE.SphereGeometry(2, 21, 21);
 var conForma = new THREE.CylinderGeometry(1,2,4,10,6,false);
 var baForma = new THREE.CylinderGeometry(2,2,0.6,10,6,false);
 var ba2Forma = new THREE.CylinderGeometry(3,3,1,20,32,false);
+var ba3Forma = new THREE.ConeGeometry( 2, 6, 32 );
 
 
 troncoForma.translate(0,8,0);
@@ -12,11 +13,13 @@ ba2Forma.translate(0,2,0);
 conForma.translate(0,10,0);
 cubierta1Forma.translate(0,1,0);
 cubierta2Forma.translate(0,13,0);
+ba3Forma.translate(0,2,0);
 
 var toncoMalla= new THREE.Mesh(troncoForma);
 var conMalla= new THREE.Mesh(conForma);
 var baMalla= new THREE.Mesh(baForma);
 var ba2Malla= new THREE.Mesh(ba2Forma);
+var ba3Malla= new THREE.Mesh(ba3Forma);
 var cubierta1Malla= new THREE.Mesh(cubierta1Forma);
 var cubierta2Malla= new THREE.Mesh(cubierta2Forma);
 
@@ -26,6 +29,7 @@ torreForma.merge(toncoMalla.geometry, toncoMalla.matrix);
 torreForma.merge(conMalla.geometry, conMalla.matrix);
 torreForma.merge(baMalla.geometry, baMalla.matrix);
 torreForma.merge(ba2Malla.geometry, ba2Malla.matrix);
+torreForma.merge(ba3Malla.geometry, ba3Malla.matrix);
 torreForma.merge(cubierta1Malla.geometry, cubierta1Malla.matrix);
 torreForma.merge(cubierta2Malla.geometry, cubierta2Malla.matrix);
 
