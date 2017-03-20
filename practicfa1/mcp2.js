f3ad4c4de67c8b0e2ec5fb0f409f65cee3fe8b25
saul <sciezk@gmail.com>
2017-03-20 17:46:17 -0600

    Update mcp2.js

--- practicfa1/mcp2.js
+++ practicfa1/mcp2.js
@@ -12,8 +12,8 @@ base3Forma.translate(0,2,0);
 troncoForma.translate(0,4,0);
 cubierta1Forma.translate(0,8,0);
 cubierta2Forma.translate(0,10,0);
-crestaForma.translate(0,10,0);
-cresta.rotateX(Math.PI/2);
+crestaForma.translate(0,12,0);
+crestaForma.rotateX(Math.PI/2);
 
 
 var base1Malla = new THREE.Mesh(base1Forma);
