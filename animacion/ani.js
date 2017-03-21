a2c268321859f30afa8815f6878659af99f29ae1
saul <sciezk@gmail.com>
2017-03-21 14:28:11 -0600

    Update ani.js

--- animacion/ani.js
+++ animacion/ani.js
@@ -12,8 +12,36 @@ malla= new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial()
 escena.add(malla);
 }
 function loop(){
+function setup(){
 
+	escena = new THREE.Scene();
+	camara = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+	camara.position.z=5;
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window-innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+	malla = new THREE.Mesh(new THREE.SphereGeometry(1), new THREEE.MeshNormalMaterial());
+	escena.add(malla);
+}
+
+
+function loop(){
+
+	requestAnimationFrame(loop);
+	
+	malla.rotation.x += 0.01;
+	malla.rotation.y += 0.01;
+	renderer.render(escena, camara);
+
+}
+
+var malla, camara, renderer, escena;
+
+setup();
+loop();
 
 malla.rotation.x
 malla.rotation.y
+ setup();
+ loop();
 }
