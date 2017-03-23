b862c444513b1eb46bd4fc3b7896c7ef604d74b1
saul <sciezk@gmail.com>
2017-03-23 13:20:20 -0600

    Update ani.js

--- animacion/ani.js
+++ animacion/ani.js
@@ -1,17 +1,3 @@
-function Setup(){
-escena=new THREE.Scan();
-camara=new THREE.PerspectiveCamera(
- 75,window.innerwidth/window.innerheight,0.1,100
- 
- );
-camara.position.==5;
-renderer=new THREE.webGLRenderer();
-renderer.setsize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderer.domElement);
-malla= new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial();
-escena.add(malla);
-}
-function loop(){
 function setup(){
 
 	escena = new THREE.Scene();
@@ -44,4 +30,4 @@ malla.rotation.x
 malla.rotation.y
  setup();
  loop();
-}
+
