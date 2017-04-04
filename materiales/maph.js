a2e57a17af2501d528ebf743b3e1ff48a962b736
saul <sciezk@gmail.com>
2017-04-04 14:03:03 -0500

    Create maph.js

---
+++ materiales/maph.js
@@ -0,0 +1,34 @@
+function setup(){
+var forma= new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshPhongMaterial({color:'#08cc00'});
+malla= new THREE.Mesh(forma, material);
+
+ var LuzPuntual= new THREE.PointLight(0xFFFFFF);
+ LuzPuntual.position.x=10;
+ LuzPuntual.position.y=10;
+ LuzPuntual.position.z=10;
+ 
+escena= new THREE.Scene();
+escena.add(malla);
+escena.add(LuzPuntual);
+
+camara= new THREE.PespectiveCamera();
+camara.position.z=5;
+
+renderer =new THREE.WebGLRenderer();
+renderer.setsize(Window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild(renderer.domElement);
+}
+function loop(){
+
+ requestAnimationFrame(loop);
+ 
+ malla.rotation.x+=0.01;
+ mallla.rotation.y +=0.01;
+ 
+ renderer.render(escena,camara);
+}
+
+var camara, escena, renderer, malla;
+setup();
+loop();
