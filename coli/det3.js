388bc8c858a8f6a9377d5bc71b9cecf9b856fc8e
saul <sciezk@gmail.com>
2017-05-25 13:24:41 -0500

    Update det3.js

--- coli/det3.js
+++ coli/det3.js
@@ -2,7 +2,7 @@ function setup(){
 
 cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
 cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-pelota=new THREE.Mesh(new THREE.SphereGeometry(0.5),new THREE.MeshNormalMaterial());
+pelota=new THREE.Mesh(new THREE.SphereGeometry(0.05),new THREE.MeshNormalMaterial());
 
 cubo1.position.x=0.7;
 cubo2.position.x=-0.7;
