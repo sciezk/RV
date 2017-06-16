527ddf6e4e057ce03860668518a0118197b57b6d
saul <sciezk@gmail.com>
2017-06-16 02:26:00 -0500

    Create ventana.js

---
+++ pff/ventana.js
@@ -0,0 +1,28 @@
+var VENTANA = new Object();
+VENTANA.listener=function(){
+  VENTANA.camara.aspect = window.innerWidth / window.innerHeight;
+  VENTANA.camara.updateProjectionMatrix();
+  VENTANA.renderizador.setSize( window.innerWidth, window.innerHeight);
+}
+VENTANA.setup=function(){
+  var tipo_evento='resize';
+  var capturarp=false;
+  window.addEventListener(tipo_evento, VENTANA.listener, capturarp); 
+  VENTANA.escena=new THREE.Scene();
+  VENTANA.camara=new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  VENTANA.camara.position.z=5;
+  var lienzo=document.getElementById("ejemplo_ventana");
+  VENTANA.renderizador=new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  VENTANA.renderizador.setSize(window.innerWidth, window.innerHeight);
+  VENTANA.malla=new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial() );
+  VENTANA.escena.add(VENTANA.malla);
+}
+VENTANA.loop=function(){
+  requestAnimationFrame(VENTANA.loop);
+  VENTANA.malla.rotateX(0.01);
+  VENTANA.malla.rotateY(0.01);
+  VENTANA.renderizador.render(VENTANA.escena, VENTANA.camara);
+}
+
+VENTANA.setup();
+VENTANA.loop();
